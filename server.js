543b18fa8d75edc40fdf2e72b09f40d866a9d4e0
Warren Longmire <warrenlongmire@gmail.com>
2016-05-26 16:45:18 -0400

    add favicon change.

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ let express = require('express');
 let app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next){
+app.use(function (req, res, next) {
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
   }
 });
+
 app.use(express.static('public'));
 
-app.listen(port, function(){
+app.listen(PORT, function(){
   console.log("Express Server is running on port " + PORT);
 });
